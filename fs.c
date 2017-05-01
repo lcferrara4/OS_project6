c1f3a7bdd857ae0469f36f4cd04941639adab7d5
Emily Obaditch <eobaditc@nd.edu>
2017-05-01 17:34:25 -0400

    fs format done

--- fs.c
+++ fs.c
@@ -7,13 +7,14 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
+#include <math.h>
 
 #define FS_MAGIC           0xf0f03410
 #define INODES_PER_BLOCK   128
 #define POINTERS_PER_INODE 5
 #define POINTERS_PER_BLOCK 1024
 
-
+int MOUNTED = 0; 
 
 struct fs_superblock {
 	int magic;
@@ -38,33 +39,64 @@ union fs_block {
 
 int fs_format()
 {
-	return 0;
+
+    union fs_block block; 
+    char super_data[DISK_BLOCK_SIZE]; 
+    int i, j; 
+
+    if(MOUNTED){
+        fprintf(stderr, "File system already mounted\n"); 
+        return 0; 
+    }
+    int blocks = disk_size(); 
+ 
+    
+    int inode_blocks = (.9 + (.1 * blocks));  
+
+
+    disk_read(0, block.data); 
+    printf("after read\n"); 
+    block.super.ninodeblocks = inode_blocks;
+    block.super.magic = FS_MAGIC; 
+    block.super.nblocks = blocks; 
+    disk_write(0, block.data); 
+
+    printf("blocks: %d\n inode_blocks: %d\n", block.super.nblocks, block.super.ninodeblocks); 
+
+    printf("after reset inode blocks\n"); 
+    for(i=0; i<blocks-1; i++){
+        disk_read(i+1, block.data);  
+        for(j=0; j<INODES_PER_BLOCK; j++){
+            block.inode[j].isvalid = 0; 
+        }
+        disk_write(i+1, block.data);
+    }
+   	return 1;
 }
 
 void fs_debug()
 {
 	union fs_block block; 
-    union fs_block indirect; 
     int i, j, k, x, y;    
     int first = 1;
 
 	disk_read(0,block.data);
 
+    if(block.super.magic != FS_MAGIC){
+        fprintf(stderr, "Magic number not valid\n"); 
+        exit(1); 
+    } 
 
 	printf("superblock:\n");
-    /*if (block.super.magic = FS_MAGIC){
-        printf("    Magic number is valid\n"); 
-    } else{
-        fprintf(stderr, "Invalid magic number\n"); 
-        exit(1); 
-    }*/
+  
 	printf("    %d blocks\n",block.super.nblocks);
 	printf("    %d inode blocks\n",block.super.ninodeblocks);
 	printf("    %d inodes\n",block.super.ninodes);
 
+    int inode_blocks = block.super.ninodeblocks; 
 
-    for (i =0; i<block.super.ninodeblocks; i++){
-        disk_read(i+1, block.data); 
+    for (i =0; i<inode_blocks; i++){
+        disk_read(i, block.data); 
         for(j=0; j<INODES_PER_BLOCK; j++){
             if(block.inode[j].isvalid == 1){
                 printf("inode: %d\n", j); 
@@ -83,15 +115,17 @@ void fs_debug()
                 if (block.inode[j].indirect != 0){
                     printf("    indirect block: %d\n", block.inode[j].indirect);
                     
-                    //TO FIX (not sure how to read indirect block yet)
-                    disk_read(block.inode[j].indirect, indirect.data); 
-                    for(x = 0; x < INODES_PER_BLOCK; x++){
-                        for(y=0; k<5; y++){
-                            if(indirect.inode[x].direct[y] != 0){
-                                printf("%d\n", indirect.inode[x].direct[y]); 
-                            }
-                        } //end for (y)
-                    } //end for (x)
+                    union fs_block indirect_info; 
+                    printf("    indirect data blocks: "); 
+                    
+                    disk_read(block.inode[j].indirect, indirect_info.data); 
+                    
+                    for(x = 0; x < POINTERS_PER_BLOCK; x++){
+                        if (indirect_info.pointers[x] != 0 ){
+                            printf("%d ", indirect_info.pointers[x]); 
+                        }
+                    } 
+                    printf("\n"); //end for (x)
                 } // end if
              } //end if 
         } // end for (j)
@@ -101,7 +135,28 @@ void fs_debug()
 
 int fs_mount()
 {
-	return 0;
+    union fs_block block; 
+    int i, j; 
+
+    disk_read(0, block.data); //read superblock 
+
+    if(block.super.magic != FS_MAGIC){
+        return 0; 
+    }
+
+    int inode_blocks = block.super.ninodeblocks; 
+    
+    for(i=0; i<inode_blocks; i++){
+        disk_read(i, block.data); 
+        for(j=0; j<INODES_PER_BLOCK; j++){
+            if (block.inode[j].isvalid == 1){
+            }
+        }
+    }
+
+    
+    MOUNTED = 1; 
+	return 1;
 }
 
 int fs_create()
