e8c2c9468af9671eda08527c5590c8a71dc038f2
Emily Obaditch <eobaditc@nd.edu>
2017-05-01 21:21:48 -0400

    fs_write todo

--- fs.c
+++ fs.c
@@ -15,6 +15,10 @@
 #define POINTERS_PER_BLOCK 1024
 
 int MOUNTED = 0; 
+int * BITMAP; 
+int NBLOCKS; 
+int CURR_BLOCK; 
+
 
 struct fs_superblock {
 	int magic;
@@ -37,49 +41,52 @@ union fs_block {
 	char data[DISK_BLOCK_SIZE];
 };
 
+void inode_load(int inumber, struct fs_inode *inode); 
+void inode_save(int inumber, struct fs_inode *inode); 
 int fs_format()
 {
 
     union fs_block block; 
-    char super_data[DISK_BLOCK_SIZE]; 
     int i, j; 
 
     if(MOUNTED){
         fprintf(stderr, "File system already mounted\n"); 
         return 0; 
     }
-    int blocks = disk_size(); 
- 
-    
-    int inode_blocks = (.9 + (.1 * blocks));  
+    NBLOCKS = disk_size(); 
+    int blocks = NBLOCKS; 
+    int inode_blocks = (.9 + (.1 * blocks)); 
+    int k; 
 
-
+    //update super block
     disk_read(0, block.data); 
-    printf("after read\n"); 
     block.super.ninodeblocks = inode_blocks;
     block.super.magic = FS_MAGIC; 
     block.super.nblocks = blocks; 
     disk_write(0, block.data); 
-
-    printf("blocks: %d\n inode_blocks: %d\n", block.super.nblocks, block.super.ninodeblocks); 
-
-    printf("after reset inode blocks\n"); 
+    
+    //clear inodes 
     for(i=0; i<blocks-1; i++){
         disk_read(i+1, block.data);  
         for(j=0; j<INODES_PER_BLOCK; j++){
-            block.inode[j].isvalid = 0; 
+            block.inode[j].isvalid = 0;
+            block.inode[j].size = 0; 
+            block.inode[j].indirect = 0; 
+            for(k=0; k<5; k++){
+                block.inode[j].direct[k] = 0; 
+            }
         }
         disk_write(i+1, block.data);
     }
+
    	return 1;
 }
 
 void fs_debug()
 {
 	union fs_block block; 
-    int i, j, k, x, y;    
-    int first = 1;
-
+    int i, j, k, x;    
+    int first = 1; 
 	disk_read(0,block.data);
 
     if(block.super.magic != FS_MAGIC){
@@ -96,7 +103,7 @@ void fs_debug()
     int inode_blocks = block.super.ninodeblocks; 
 
     for (i =0; i<inode_blocks; i++){
-        disk_read(i, block.data); 
+        disk_read(i+1, block.data); 
         for(j=0; j<INODES_PER_BLOCK; j++){
             if(block.inode[j].isvalid == 1){
                 printf("inode: %d\n", j); 
@@ -136,24 +143,33 @@ void fs_debug()
 int fs_mount()
 {
     union fs_block block; 
-    int i, j; 
+    int i, j, inuse; 
 
     disk_read(0, block.data); //read superblock 
-
     if(block.super.magic != FS_MAGIC){
         return 0; 
     }
 
     int inode_blocks = block.super.ninodeblocks; 
-    
-    for(i=0; i<inode_blocks; i++){
+
+    BITMAP = (int *)malloc(sizeof(int)*NBLOCKS); 
+
+    BITMAP[0] = 1; //superblock 
+
+    for(i=1; i<inode_blocks; i++){
+        inuse = 0; 
         disk_read(i, block.data); 
         for(j=0; j<INODES_PER_BLOCK; j++){
             if (block.inode[j].isvalid == 1){
+               inuse += 1;
             }
         }
+        if (inuse == INODES_PER_BLOCK){
+            BITMAP[i] = 1; 
+        } else{
+            BITMAP[i] = 0; 
+        }
     }
-
     
     MOUNTED = 1; 
 	return 1;
@@ -161,25 +177,172 @@ int fs_mount()
 
 int fs_create()
 {
-	return 0;
+	int i; 
+    union fs_block block; 
+    int free_block = -1; 
+    int inumber = -1; 
+
+    for(i=1; i<NBLOCKS; i++){
+        if(BITMAP[i] == 0){
+            free_block = i; 
+            break; 
+        }
+    }
+    if (free_block == -1){
+        fprintf(stderr, "No free blocks\n"); 
+        return 0; 
+    }
+    
+    disk_read(free_block, block.data);
+
+    for(i=1; i<INODES_PER_BLOCK; i++){
+        if(block.inode[i].isvalid == 0){
+            inumber = (free_block-1)*INODES_PER_BLOCK + i; 
+            printf("inumber: %d\n", inumber); 
+            block.inode[i].isvalid = 1; 
+            disk_write(free_block, block.data); 
+            break; 
+        }
+    }
+    if (inumber == -1){
+        fprintf(stderr, "no valid inodes\n"); 
+        return 0; 
+    }
+    if(inumber == INODES_PER_BLOCK -1){
+        BITMAP[free_block] = 1; 
+    }
+    return inumber; 
 }
 
 int fs_delete( int inumber )
 {
-	return 0;
+
+    if(!MOUNTED){
+        fprintf(stderr, "File system not mounted\n"); 
+        return -1; 
+    }
+ 
+    struct fs_inode  curr; 
+    int i;  
+    
+    inode_load(inumber, &curr); 
+    
+    if (curr.isvalid ==0){
+        fprintf(stderr, "Error in deleting inode: does not exist\n"); 
+        return 0; 
+    }
+
+    curr.isvalid = 0; 
+    curr.size = 0; 
+    curr.indirect = 0; 
+    for(i=0; i<5; i++){
+        curr.direct[i] = 0; 
+    }
+    inode_save(inumber, &curr); 
+    
+    BITMAP[CURR_BLOCK]=0; 
+
+    return 1;
 }
 
 int fs_getsize( int inumber )
 {
-	return -1;
+    if(!MOUNTED){
+        fprintf(stderr, "File system not mounted\n"); 
+        return -1; 
+    }
+    
+    struct fs_inode curr; 
+    inode_load(inumber, &curr); 
+
+    if (curr.isvalid == 0){
+        fprintf(stderr, "Error: inode does not exist\n"); 
+	    return -1;
+    } 
+
+    return curr.size; 
 }
 
 int fs_read( int inumber, char *data, int length, int offset )
 {
-	return 0;
+	
+    if(!MOUNTED){
+        fprintf(stderr, "File system not mounted\n"); 
+        return 0; 
+    }
+    
+    int i; 
+    union fs_block block; 
+    struct fs_inode curr; 
+    inode_load(inumber, &curr); 
+
+    if(curr.isvalid == 0){
+        fprintf(stderr, "Inode does not exist\n"); 
+        return 0; 
+    }
+    
+    if (offset >= curr.size){
+        fprintf(stderr, "Error:Offset greater than or equal to file size\n");  
+        return 0; 
+    }
+
+
+    int curr_byte = 0; 
+    int count = 0; 
+    int j = 0; 
+    int bytes_read = 0; 
+    
+    while(bytes_read < length){
+        if(count == 5){
+            disk_read(curr.indirect, block.data); 
+        }else {
+            disk_read(curr.direct[count], block.data); 
+        }
+        for(i=0; i<DISK_BLOCK_SIZE; i++){
+            curr_byte +=1; 
+            if(curr_byte >= offset){
+                data[j] = block.data[i]; 
+                j++; 
+                bytes_read += 1; 
+                if(bytes_read == length){
+                    return bytes_read; 
+                }
+            }
+        }
+        count++; 
+        if(count ==6){
+            break; 
+        }
+    }
+    
+    return bytes_read; 
 }
 
 int fs_write( int inumber, const char *data, int length, int offset )
 {
 	return 0;
+}
+
+void inode_load(int inumber, struct fs_inode * fs){
+    
+    union fs_block block; 
+    CURR_BLOCK = (inumber / INODES_PER_BLOCK) + 1; 
+    int i = inumber - (INODES_PER_BLOCK * (CURR_BLOCK-1)); 
+
+    disk_read(CURR_BLOCK, block.data); 
+
+    *fs = block.inode[i]; 
+
+}
+
+void inode_save(int inumber, struct fs_inode * fs){
+
+    CURR_BLOCK = (inumber / INODES_PER_BLOCK) +1; 
+    union fs_block block; 
+    int i = inumber - (INODES_PER_BLOCK * (CURR_BLOCK-1)); 
+
+    disk_read(CURR_BLOCK, block.data); 
+    block.inode[i] = *fs; 
+    disk_write(CURR_BLOCK, block.data); 
+
 }
