3a999b4bba339be6626aae78104b263bfcafc20b
Lauren Christine Ferrara <lferrara@student01.cse.nd.edu>
2017-05-03 10:32:22 -0400

    changed create/mount/format

--- fs_old.c
+++ fs_old.c
@@ -21,8 +21,7 @@ int * INODE_BITMAP;
 int * INUMBERS;
 int NBLOCKS; 
 int CURR_BLOCK; 
-int * NEXT_AVAILABLE; 
-int TOTAL_INUMBERS;
+int NEXT_AVAILABLE =1; 
 
 
 struct fs_superblock {
@@ -49,8 +48,6 @@ union fs_block {
 void inode_load(int inumber, struct fs_inode *inode); 
 void inode_save(int inumber, struct fs_inode *inode); 
 int determine_block(int inumber, int offset); 
-int get_NEXT_AVAILABLE();
-void release_inumber(int inumber);
 
 int fs_format(){
 
@@ -105,17 +102,14 @@ void fs_debug()
 
     int inode_blocks = block.super.ninodeblocks; 
 
-	int start_inode = 2;
     for (i =0; i<inode_blocks; i++){
         disk_read(i+1, block.data); 
         for(j=0; j<INODES_PER_BLOCK; j++){
             if(block.inode[j].isvalid == 1){
                 if(!MOUNTED){
-			int curr_inode = 2;
-                    printf("inode: %d\n", start_inode);   
-			start_inode++;
+                    printf("inode: %d\n", j);   
                 } else{
-                    printf("inode: %d\n", INUMBERS[i * INODES_PER_BLOCK + j]); 
+                    printf("inode: %d\n", INUMBERS[j]); 
                 } printf("    size: %d bytes\n", block.inode[j].size); 
                 for(k=0; k<5; k++){
                     if (block.inode[j].direct[k] != 0){
@@ -156,7 +150,6 @@ int fs_mount()
 
     disk_read(0, block.data); //read superblock 
     if(block.super.magic != FS_MAGIC){
-	printf("not super\n");
         return 0; 
     }
 
@@ -167,28 +160,15 @@ int fs_mount()
 
     INODE_BITMAP = (int *)malloc(sizeof(int)*NBLOCKS*INODES_PER_BLOCK); 
     INUMBERS = (int *)malloc(sizeof(int)*NBLOCKS*INODES_PER_BLOCK); 
+    BITMAP[0] = 1; //superblock 
 
-	TOTAL_INUMBERS = 1 + inode_blocks * (POINTERS_PER_INODE + INODES_PER_BLOCK * (1 + POINTERS_PER_BLOCK) );
-	NEXT_AVAILABLE = (int *)malloc(sizeof(int)*TOTAL_INUMBERS);  
-	for( i = 0; i < TOTAL_INUMBERS; i++ ){
-		NEXT_AVAILABLE[i] = 0;
-	}
- 
-	BITMAP[0] = 1; //superblock 
-	NEXT_AVAILABLE[0] = 1;
-	NEXT_AVAILABLE[1] = 1;
-	int inumber;
-
-    for(i=1; i<=inode_blocks; i++){
+    for(i=1; i<inode_blocks; i++){
         inuse = 0; 
         disk_read(i, block.data); 
         for(j=0; j<INODES_PER_BLOCK; j++){
             if (block.inode[j].isvalid == 1){
                 inuse += 1;
-                inumber = get_NEXT_AVAILABLE();
-	            BLOCK_BITMAP[inumber] = i;
-    	        INODE_BITMAP[inumber] = j;
-        	    INUMBERS[i] = inumber;
+                INUMBERS[j] = NEXT_AVAILABLE; NEXT_AVAILABLE++; //ADDED 
             }
         }
         if (inuse == INODES_PER_BLOCK){
@@ -198,46 +178,22 @@ int fs_mount()
         }
     }
     
-    int k, p; 
-	int start_inode = 2;
-    for(i=1; i<= inode_blocks; i++){
-        disk_read(i, block.data);  
-        for(j=0; j<INODES_PER_BLOCK; j++){
-		/*
-		if (block.inode[j].isvalid == 0){
-			printf("not valid\n");
-		    block.inode[j].size = 0; 
-		    block.inode[j].indirect = 0; 
-		    for(k=0; k < POINTERS_PER_INODE; k++){
-			block.inode[j].direct[k] = 0; 
-		    }
-			disk_write(i, block.data);
-		}
-		else{
-		*/
-		if( block.inode[j].isvalid == 1){
-			// already in use
-			NEXT_AVAILABLE[start_inode] = 1;
-			start_inode++;
-			for(k=0; k < POINTERS_PER_INODE; k++){
-				if(block.inode[j].direct[k] != 0){
-					NEXT_AVAILABLE[block.inode[j].direct[k]] = 1;
-				}
-			}
-			if(block.inode[j].indirect != 0 ){
-				NEXT_AVAILABLE[block.inode[j].indirect] = 1;
-				disk_read(block.inode[j].indirect, block.data);
-				for(p = 0; p < INODES_PER_BLOCK; p++ ){
-					if( block.pointers[p] != 0 ){
-						NEXT_AVAILABLE[block.pointers[p]] = 1;
-						//disk_read(block.pointers[p], block.data);
-					}
-				}
-
-			}
-		}
+    int k; 
+    for(i=0; i<NBLOCKS-1; i++){
+        disk_read(i+1, block.data);  
+        if (block.inode[j].isvalid == 0){
+            block.inode[j].size = 0; 
+            block.inode[j].indirect = 0; 
+            for(k=0; k<5; k++){
+                block.inode[j].direct[k] = 0; 
+            }
+        disk_write(i+1, block.data);
+        }
+        else{
+            NEXT_AVAILABLE++; 
         }
     }
+    printf("NEXT_AVAILABLE %d\n", NEXT_AVAILABLE); 
 
     
     MOUNTED = 1; 
@@ -268,11 +224,11 @@ int fs_create()
 
     for(i=1; i<INODES_PER_BLOCK; i++){
         if(block.inode[i].isvalid == 0){
-            inumber = get_NEXT_AVAILABLE(); 
-			printf("inumber: %d\n", inumber);
-            BLOCK_BITMAP[inumber] = free_block;
-            INODE_BITMAP[inumber] = i;
-            INUMBERS[i] = inumber;
+            inumber = NEXT_AVAILABLE; 
+            BLOCK_BITMAP[NEXT_AVAILABLE] = free_block;
+            INODE_BITMAP[NEXT_AVAILABLE] = i;
+            INUMBERS[i] = NEXT_AVAILABLE;
+            NEXT_AVAILABLE++;
 
             block.inode[i].isvalid = 1;
             block.inode[i].size = 0; 
@@ -324,7 +280,6 @@ int fs_delete( int inumber )
     BITMAP[CURR_BLOCK]=0; 
     INODE_BITMAP[inumber] = -1;
     BLOCK_BITMAP[inumber] = -1;
-	release_inumber(inumber);
 
     return 1;
 }
@@ -366,7 +321,7 @@ int fs_read( int inumber, char *data, int length, int offset )
     }
     
     if (offset >= curr.size){
-    	// stops reading from shell
+    // stops reading from shell
         return 0; 
     }
     if(curr.size == 0){
@@ -378,39 +333,62 @@ int fs_read( int inumber, char *data, int length, int offset )
     int block_pointer = determine_block(inumber, offset); 
     int bytes_read = 0; 
     int bytes_read_curr = 0;
-    int x, curr_indirect_block;
+	int x, curr_indirect_block;
 
     while(bytes_read < length){
-		// indirect
+	printf("block pointer = %d\n", block_pointer);
         if(block_pointer >= 5){
+		printf("in indirect\n");
+		printf("curr indirect: %d\n", curr.indirect);
             disk_read(curr.indirect, block.data); 
+		printf("block pointer 5: %d\n", block.pointers[block_pointer - 5]);
             if( block.pointers[block_pointer - 5] != 0 ){
                 curr_indirect_block = block.pointers[block_pointer - 5];
+		printf("curr indirect block: %d\n", curr_indirect_block);
             } else{
+		printf("bytes_read before return: %d\n", bytes_read);
                 return bytes_read; // no more data to read
             }
-	    	disk_read(curr_indirect_block, block.data);
-        } else { // direct
+		printf("curr indirect block: %d\n", curr_indirect_block);
+		disk_read(curr_indirect_block, block.data);
+        } else {
+		printf("in direct\n");
             disk_read(curr.direct[block_pointer], block.data); 
-        }
-		// copy data 
+        }  
+        printf("block data: %s\n", block.data); 
         if ( bytes_read + DISK_BLOCK_SIZE > length ){
+		printf("1\n");
             memcpy(data + offset + bytes_read, block.data, length - bytes_read);
             bytes_read_curr = length - bytes_read;
             bytes_read = length; 
     	} else if( curr.size - (offset + bytes_read) < DISK_BLOCK_SIZE){
+
+		printf("3\n");
+		printf("offset: %d, bytes_read: %d\n", offset, bytes_read);
+		/*for( x = 0; x < curr.size - offset - bytes_read; x++ ){
+			data[x+offset + bytes_read] = block.data[x];
+			printf("block data[x]: %c\n", block.data[x]);
+		}*/
             memcpy(data+offset+bytes_read, block.data, curr.size - offset - bytes_read); 
             bytes_read_curr = curr.size - offset - bytes_read;
             bytes_read = length; 
 
-		} else{
-            memcpy(data + offset + bytes_read, block.data, DISK_BLOCK_SIZE);
-			for( x = 0; x < DISK_BLOCK_SIZE; x++ ){
-				data[x+offset+bytes_read] = block.data[x];
-			}	
+	} else{
+		printf("2\n");
+		printf("offset: %d, bytes_read: %d\n", offset, bytes_read);
+            	memcpy(data + offset + bytes_read, block.data, DISK_BLOCK_SIZE);
+		/*
+		for( x = 0; x < DISK_BLOCK_SIZE; x++ ){
+			data[x+offset+bytes_read] = block.data[x];
+		}	
+		*/
             bytes_read_curr = DISK_BLOCK_SIZE;
+            //bytes_read_curr = strlen(block.data);
             bytes_read += bytes_read_curr; 
     	}
+	printf("bytes_read_curr: %d\n", bytes_read_curr);
+	printf("bytes_read: %d\n", bytes_read);
+    
     	if(bytes_read >= length ){
         	printf("Stopped here\n");
         	if( curr.size < bytes_read )
@@ -426,6 +404,7 @@ int fs_read( int inumber, char *data, int length, int offset )
 
 int fs_write( int inumber, const char *data, int length, int offset )
 {
+	printf("data: %s\n", data);
     struct fs_inode curr; 
     union fs_block block; 
     int x; 
@@ -447,64 +426,69 @@ int fs_write( int inumber, const char *data, int length, int offset )
         return 0; 
     } 
 
-    int disk_offset = offset % DISK_BLOCK_SIZE;
     int bytes_written = 0; 
     int curr_indirect_block; 
-    while(length > 0){ // && block_pointer < something
-        if(block_pointer == 5){ // creates space for indirect
-            curr.indirect = get_NEXT_AVAILABLE();
+    int j=0; 
+    while(bytes_written < length){
+        if(block_pointer == 5){
+            curr.indirect = NEXT_AVAILABLE;
             disk_read(curr.indirect, block.data); 
             for(x=0; x<POINTERS_PER_BLOCK; x++){
                 block.pointers[x] = 0; 
             }
             disk_write(curr.indirect, block.data); 
+            NEXT_AVAILABLE++; 
         }
-        if(block_pointer >= 5){ // indirect
+        if(block_pointer >= 5){
             disk_read(curr.indirect, block.data); 
             for(x=0; x<POINTERS_PER_BLOCK; x++){
                 if(block.pointers[x] == 0){
-                    block.pointers[x] = get_NEXT_AVAILABLE();
+                    block.pointers[x] = NEXT_AVAILABLE;
                     disk_write(curr.indirect, block.data);
+                    NEXT_AVAILABLE++; 
                     curr_indirect_block = block.pointers[x]; 
                     break; 
                 }
             }
             disk_read(curr_indirect_block, block.data); 
-        }else { // direct
-            curr.direct[block_pointer] = get_NEXT_AVAILABLE(); 
+        }else {
+            curr.direct[block_pointer] = NEXT_AVAILABLE; 
             disk_read(curr.direct[block_pointer], block.data); 
+            NEXT_AVAILABLE++; 
         }
-		// copy data
-		int k;
-		if ( DISK_BLOCK_SIZE > length ){
-			memcpy(block.data, data + offset + bytes_written, length);
-			bytes_written += length;
-			length = 0;
-			
-			/*
-				for( k = 0; k < length - bytes_written -offset; k ++ ){
-					block.data[k] = data[k+offset+bytes_written];
-				}
-
-				printf("data in 1: %c\n", data[bytes_written + offset+k]); 
-				//printf("block data from 1: %s\n", block.data); 
-				bytes_written = length;
-			*/
-		} else{
-			memcpy(block.data, data +offset + bytes_written, DISK_BLOCK_SIZE);
-			bytes_written += DISK_BLOCK_SIZE;
-			length -= DISK_BLOCK_SIZE;
+	// copy data
+	int k;
+	if ( bytes_written + DISK_BLOCK_SIZE > length ){
+		printf("1\n");
+        printf("data in 1: %c\n", data[bytes_written + offset+k]); 
+		//memcpy(block.data, data + bytes_written + offset, length - bytes_written);
+		for( k = 0; k < length - bytes_written -offset; k ++ ){
+			block.data[k] = data[k+offset+bytes_written];
 		}
-		// write data to disk
+
+        printf("data in 1: %c\n", data[bytes_written + offset+k]); 
+        //printf("block data from 1: %s\n", block.data); 
+		bytes_written = length;
+	} else{
+		printf("2\n");
+        printf("data in 2: %c\n", data[bytes_written + offset]); 
+		memcpy(block.data, data +offset+bytes_written, DISK_BLOCK_SIZE);
+
+
+        printf("data in 2: %c\n", data[bytes_written + offset]); 
+        //printf("block data from 2: %s\n", block.data); 
+		bytes_written += DISK_BLOCK_SIZE;
+	}
+        //update inode size
         if(block_pointer >= 5){
             disk_write(curr_indirect_block, block.data); 
         }else {
             disk_write(curr.direct[block_pointer], block.data); 
         }
-        //update inode size
         if(bytes_written == length){
             curr.size += bytes_written;
             inode_save(inumber, &curr);
+         //   printf("block data from write: %s\n", block.data); 
             return bytes_written; 
         } 
             
@@ -541,34 +525,15 @@ int determine_block(int inumber, int offset){
         return  -1; 
     }
 
-    //if(offset % 4096 == 0){
+    if(offset % 4096 == 0){
             block_num = offset / 4096; 
-    //} else{
-    //    block_num = offset / 4096 + 1;
-    //}
+    } else{
+        block_num = offset / 4096 + 1;
+    }
     
     //if(block_num >4){
  //   block_num =5; 
     //}
 
     return block_num; 
-}
-
-int get_NEXT_AVAILABLE(){
-    union fs_block block; 	
-	disk_read(0, block.data); 
-    int inode_blocks = block.super.ninodeblocks;
-	
-	int i;
-	for ( i = 0; i < TOTAL_INUMBERS; i++ ){
-		if( NEXT_AVAILABLE[i] == 0 ){
-			NEXT_AVAILABLE[i] = 1;
-			return i;
-		}
-	}
-	return -1; // completely full
-}
-
-void release_inumber(int inumber){
-	NEXT_AVAILABLE[inumber] = 0;
 }
---
+++ test.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+HOSTDIR="/afs/nd.edu/user21/jwesthof/Public"
+uut="./simplefs"
+cat <<EOF
+RUNNING LAME TEST SCRIPT VERSION 7
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Please note that this is not a comprehensive test, it just tests
+a couple of corner cases. If you have any questions, comments, concerns,
+suggestions, or whatever, bug @jeffrey on slack, or John Westhoff on facebook.
+
+EOF
+tmp=`mktemp -d`
+chmod 777 $tmp
+cp $HOSTDIR/test/image.5 $tmp/img.5
+
+### TEST ONE ###
+$uut $tmp/img.5 5 >/dev/null <<EOF
+mount
+copyout 1 $tmp/1.txt
+EOF
+## Verify test succeeded ##
+if diff $tmp/1.txt $HOSTDIR/test/1.txt ; then
+    echo "TEST ONE GOOD - Simple copyout works"
+else
+    echo "TEST ONE FAIL - Copying out a file failed"
+fi
+
+### TEST TWO ###
+$uut $tmp/test.5 5 > /dev/null <<EOF
+format
+mount
+create
+create
+create
+copyin $HOSTDIR/test/hi 1
+copyin $HOSTDIR/test/hi 2
+copyin $HOSTDIR/test/hi 3
+copyout 1 $tmp/1.txt
+copyout 2 $tmp/2.txt
+copyout 3 $tmp/3.txt
+cat 3
+EOF
+if diff $tmp/1.txt $HOSTDIR/test/hi > /dev/null && \
+   diff $tmp/2.txt $HOSTDIR/test/hi > /dev/null && \
+   diff $tmp/3.txt $HOSTDIR/test/hi > /dev/null ; then
+    echo "TEST TWO GOOD - Create and Copyout work"
+else
+    echo "TEST TWO FAIL - Copying in and out three files failed"
+fi
+
+### TEST THREE ###
+$uut $tmp/test.5 5 > /dev/null <<EOF
+format
+mount
+create
+create
+create
+create
+copyin $HOSTDIR/test/hi 1
+copyin $HOSTDIR/test/hi 2
+copyin $HOSTDIR/test/hi 3
+copyin $HOSTDIR/test/hi 4
+copyout 1 $tmp/1.txt
+copyout 2 $tmp/2.txt
+copyout 3 $tmp/3.txt
+copyout 4 $tmp/4.txt
+EOF
+if diff $tmp/1.txt $HOSTDIR/test/hi > /dev/null && \
+   diff $tmp/2.txt $HOSTDIR/test/hi > /dev/null && \
+   diff $tmp/3.txt $HOSTDIR/test/hi > /dev/null && \
+   [ ! -s $tmp/4.txt ] ; then
+    echo "TEST THREE GOOD - Filling the disk and then copying out worked"
+else
+    echo "TEST THREE FAIL - Filling the disk failed and copyin/out failed"
+fi
+
+### TEST FOUR ###
+$uut $tmp/img.1222 1222 > /dev/null <<EOF
+format
+mount
+create
+copyin $HOSTDIR/test/big 1
+copyout 1 $tmp/big
+debug
+EOF
+## Verify test succeeded ##
+if diff $tmp/big $HOSTDIR/test/big > /dev/null ; then
+    echo "TEST FOUR GOOD - Copying in and out a really big file worked"
+else
+    echo "TEST FOUR FAIL - Filling a really big file failed"
+fi
+
+### TEST FIVE ###
+$uut $tmp/img.1222 1222 > /dev/null <<EOF
+format
+mount
+create
+copyin $HOSTDIR/test/big 1
+delete 1
+create
+copyin $HOSTDIR/test/big 1
+copyout 1 $tmp/big
+debug
+EOF
+## Verify test succeeded ##
+if diff $tmp/big $HOSTDIR/test/big > /dev/null ; then
+    echo "TEST FIVE GOOD - Copying in, deleting, and recreating a big file"
+else
+    echo "TEST FIVE FAIL - Filling and deleting a really big file failed"
+fi
+
+### TEST SIX ##
+$uut $tmp/img.11 11 > /dev/null <<EOF
+format
+mount
+`for i in {1..128} ; do echo "create" ; done`
+copyin $HOSTDIR/test/hi 128
+copyout 128 $tmp/hi2
+debug
+EOF
+## Verify test succeeded ##
+if diff $tmp/hi2 $HOSTDIR/test/hi > /dev/null ; then
+    echo "TEST SIX GOOD - Creating many inodes and copying in/out works"
+else
+    echo "TEST SIX FAIL - Creating many inodes and copying out failed"
+fi
+
+### TEST SEVEN ##
+$uut $tmp/img.10 10 > /dev/null <<EOF
+format
+mount
+`for i in {1..128} ; do echo "create" ; done`
+copyin $HOSTDIR/test/hi 128
+copyout 128 $tmp/how
+debug
+EOF
+## Verify test succeeded ##
+if [ ! -s $tmp/how ] ; then
+    echo "TEST SEVEN GOOD - Creating too many inodes seems to be handled well"
+else
+    echo "TEST SEVEN FAIL - Creating too many inodes failed"
+fi
+
+### TEST EIGHT ##
+T8A=`$uut $tmp/img.create 10 <<EOF
+format
+mount
+debug
+EOF`
+T8B=`$uut $tmp/img.create 10 <<EOF
+format
+mount
+create
+debug
+EOF`
+## Verify test succeeded ##
+if [ `echo $T8A | wc -w` -lt `echo $T8B | wc -w` ] ; then
+    echo "TEST EIGHT GOOD - Create does something"
+else
+    echo "TEST EIGHT FAIL - Create Failed"
+fi
+
+### TEST NINE ##
+T9A=`$uut $tmp/img.create 10 <<EOF
+format
+mount
+debug
+EOF`
+T9B=`$uut $tmp/img.create 10 <<EOF
+format
+mount
+create
+create
+debug
+EOF`
+T9C=`$uut $tmp/img.create 10 <<EOF
+format
+mount
+create
+create
+delete 2
+debug
+EOF`
+## Verify test succeeded ##
+if [ `echo $T9A | wc -w` -lt `echo $T9C | wc -w` ] && 
+   [ `echo $T9C | wc -w` -lt `echo $T9B | wc -w` ] ; then
+    echo "TEST NINE GOOD - Create create delete seems to work"
+else
+    echo "TEST NINE FAIL - Create create delete failed"
+fi
+
+### TEST TEN ##
+$uut $tmp/img.create 10 > /dev/null <<EOF
+format
+EOF
+T10A=`$uut $tmp/img.create 10 <<EOF
+mount
+debug
+EOF`
+$uut $tmp/img.create 10 > /dev/null <<EOF
+mount
+create
+create
+delete 1
+delete 2
+debug
+EOF
+T10B=`$uut $tmp/img.create 10 <<EOF
+mount
+debug
+EOF`
+## Verify test succeeded ##
+echo $T10A
+echo $T10B
+if [ "$T10A" = "$T10B" ] ; then
+    echo "TEST TEN GOOD - Create create delete delete seems to work"
+else
+    echo "TEST TEN FAIL - Create create delete delete failed"
+fi
+
+### TEST 11 ##
+$uut $tmp/img.defrag 2000 > /dev/null <<EOF
+format
+mount
+`for i in {1..384} ; do echo "create" ; done`
+`for i in {2..384..2} ; do echo "copyin $HOSTDIR/test/hi $i" ; done`
+copyin $HOSTDIR/test/big 1
+defrag
+copyout 1 $tmp/biggest
+EOF
+## Verify test succeeded ##
+if diff $tmp/biggest $HOSTDIR/test/big > /dev/null ; then
+    echo "TEST 11 GOOD - defrag doesn't break anything"
+else
+    echo "TEST 11 FAIL - defrag breaks something"
+fi
